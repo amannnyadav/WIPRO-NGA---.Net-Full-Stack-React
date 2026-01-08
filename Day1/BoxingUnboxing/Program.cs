90088c36d726aaa92f7620c7885785773471c95a
aman <av2082@srmist.edu.in>
2026-01-08 18:41:54 +0530

    Day1 commit

--- Day1/BoxingUnboxing/Program.cs
+++ Day1/BoxingUnboxing/Program.cs
@@ -1,3 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hi");
+using System.Security.AccessControl;
+
+int num=2;
+Object obj=num;
+Console.WriteLine(obj);
+int number=(int)obj;
+Console.WriteLine(number);
+
 
