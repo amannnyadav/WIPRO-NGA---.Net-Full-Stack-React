cc31900fe127360b69da1ac02fd8aca0c622391f
aman <av2082@srmist.edu.in>
2026-01-13 09:39:03 +0530

    Day4 added

---
+++ Day4/StaticDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace StaticDemo
+{
+    public static class MathUtilities
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Static Demo");
+            int sum = MathUtilities.Add(5, 3);
+            int difference = MathUtilities.Subtract(10, 4); 
+            Console.WriteLine($"Sum: {sum}");
+            Console.WriteLine($"Difference: {difference}");
+        }
+    }
+}
